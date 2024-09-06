af938a8b012b29a6e3591685569844522f8ff9ea
Bui Tuan Son <SonBTHE172207@fpt.edu.vn>
2024-09-06 20:18:07 +0700

    chore:update git flow

--- frontend/src/utils/env.ts
+++ frontend/src/utils/env.ts
@@ -1,12 +1,33 @@
 const META = {
-  BASE_URL: (import.meta.env.PUBLIC_ENV__META__BASE_URL ?? 'http://localhost:3000') as string,
-  DEFAULT_AUTHOR: (import.meta.env.PUBLIC_ENV__META__DEFAULT_AUTHOR ??
-    'IT Team 4 Change') as string,
-  DEFAULT_DESCRIPTION: (import.meta.env.PUBLIC_ENV__META__DEFAULT_DESCRIPTION ??
-    'IT4C Frontend Boilerplate') as string,
-  DEFAULT_TITLE: (import.meta.env.PUBLIC_ENV__META__DEFAULT_TITLE ?? 'IT4C') as string,
-  BACKEND: (import.meta.env.PUBLIC_ENV__META__BACKEND ?? 'http://localhost:8080') as string,
-  CHECKSUM_KEY: (import.meta.env.PUBLIC_ENV__META__CHECKSUM_KEY ?? '40612359b37772321b7') as string,
+  BASE_URL:
+    (typeof import.meta !== 'undefined' && import.meta.env?.PUBLIC_ENV__META__BASE_URL) ||
+    process.env.PUBLIC_ENV__META__BASE_URL ||
+    'http://localhost:3000',
+
+  DEFAULT_AUTHOR:
+    (typeof import.meta !== 'undefined' && import.meta.env?.PUBLIC_ENV__META__DEFAULT_AUTHOR) ||
+    process.env.PUBLIC_ENV__META__DEFAULT_AUTHOR ||
+    'IT Team 4 Change',
+
+  DEFAULT_DESCRIPTION:
+    (typeof import.meta !== 'undefined' && import.meta.env?.PUBLIC_ENV__META__DEFAULT_DESCRIPTION) ||
+    process.env.PUBLIC_ENV__META__DEFAULT_DESCRIPTION ||
+    'IT4C Frontend Boilerplate',
+
+  DEFAULT_TITLE:
+    (typeof import.meta !== 'undefined' && import.meta.env?.PUBLIC_ENV__META__DEFAULT_TITLE) ||
+    process.env.PUBLIC_ENV__META__DEFAULT_TITLE ||
+    'IT4C',
+
+  BACKEND:
+    (typeof import.meta !== 'undefined' && import.meta.env?.PUBLIC_ENV__META__BACKEND) ||
+    process.env.PUBLIC_ENV__META__BACKEND ||
+    'http://localhost:8080',
+
+  CHECKSUM_KEY:
+    (typeof import.meta !== 'undefined' && import.meta.env?.PUBLIC_ENV__META__CHECKSUM_KEY) ||
+    process.env.PUBLIC_ENV__META__CHECKSUM_KEY ||
+    '40612359b37772321b7',
 }
 
 export { META }
